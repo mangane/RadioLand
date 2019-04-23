e075f61bb07c161655f02677341ca12420404d89
mangane <40431348+mangane@users.noreply.github.com>
2019-04-23 16:51:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = ".";
+client.on('ready', () => {
+client.user.setStatus('idle')
+  .then(console.log)
+  .catch(console.error);
 });
 client.login(process.env.BOT_TOKEN)
