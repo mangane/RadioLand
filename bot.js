a29a7f9d4f33d19da33ed1092e0abc629f049cac
mangane <40431348+mangane@users.noreply.github.com>
2019-04-23 18:35:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,10 +14,7 @@ if(message.content.startsWith(prefix + `anim`)) {
         const count = new Discord.RichEmbed()
         .setColor(0x954D23)
         .setTitle("**Voici les animations du serveur :**")
-    .addField(":Satyre59: @🔉| Satyre 59 ","-Dimanche 8h30-10h00-Vendredi 20h00-22h00-Samedi 20h00-21h00"}
-        .addField("Date de création du serveur",  message.guild.createdAt)
-    .addField("Vous avez rejoint le serveur",  message.member.joinedAt)
-        .addField("Total des membres :",  message.guild.memberCount);
+    .addField(":Satyre59: @🔉| Satyre 59 ","Dimanche 8h30-10h00/Vendredi 20h00-22h00/Samedi 20h00-21h00"};
         return message.channel.send(count)
     }
 });
