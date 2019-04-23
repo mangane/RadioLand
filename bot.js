7f5d22c9bbad56db843f710aab97a57986a21916
mangane <40431348+mangane@users.noreply.github.com>
2019-04-23 20:18:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,8 +9,14 @@ client.user.setStatus('idle')
 client.on('ready', () => {
   client.user.setActivity("Maintenance en cours");
   });
-if(message.channel.startsWith(prefix + "test")) {
-    message.reply ("ceci est un  test");
-}
+if(message.content.startsWith(prefix + "invite")) {
+			message.delete()
+		const embed = new Discord.RichEmbed()		
+			.setColor(0x954D23)
+			.setTitle("Invitation :")
+			.addField("Voici l'email :","gaetanoverbot@gmail.com")
+			.addField("Voici le lien pour m'inviter","https://discordapp.com/api/oauth2/authorize?client_id=520322405982535705&permissions=8&scope=bot");
+		message.channel.send({embed});
+} 
 });
 client.login(process.env.BOT_TOKEN)
