c6a7eb819bd139210d65c4284a7436796cf0cc4f
mangane <40431348+mangane@users.noreply.github.com>
2019-04-23 17:00:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,4 +6,19 @@ client.user.setStatus('idle')
   .then(console.log)
   .catch(console.error);
 });
+client.on('ready', () => {
+  client.user.setActivity("Maintenance en cours");
+  });
+if(message.content.startsWith(prefix + `infoserv`)) {
+        message.delete()
+        const count = new Discord.RichEmbed()
+        .setColor(0x954D23)
+        .setTitle("**Information du serveur **")
+    .addField("Nom du serveur", message.guild.name)
+        .addField("Date de création du serveur",  message.guild.createdAt)
+    .addField("Vous avez rejoint le serveur",  message.member.joinedAt)
+        .addField("Total des membres :",  message.guild.memberCount);
+        return message.channel.send(count)
+    }
+});
 client.login(process.env.BOT_TOKEN)
