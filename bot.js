8ddb272eb29e176f493c37777e1a06ca52f919c7
mangane <40431348+mangane@users.noreply.github.com>
2019-04-25 14:50:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,11 +10,14 @@ client.on('ready', () => {
   client.user.setActivity("Maintenance en cours");
   });
   client.on(`message`, message =>{
-    if(message.content.startsWith(prefix + "test")) {
-      message.reply("test");
-      }
-    if(message.content.startsWith(prefix + "allo")) {
-      message.channel.send("hello")
-      }
+    if(message.content.startsWith(prefix + "anim")) {
+        message.delete()
+        const embed = new Discord.RichEmbed()
+        .setColor("#2e1fe6")
+        .setTitle("Voici les animateurs de RadioLand avec leurs horaires :")
+        .addField(":Satyre59: Satyre59 :","```Dimanche 8h30-10h00\nVendredi 20h00-22h00\n Samedi 20h00-21h00```")
+        .setFooter("RadioLand");
+         message.channel.send({embed})
+    }
   });
 client.login(process.env.BOT_TOKEN)
