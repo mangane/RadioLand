23751cbb0214c2affa4e8ec9ec97806377d93b86
mangane <40431348+mangane@users.noreply.github.com>
2019-04-25 14:56:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('ready', () => {
         .setColor("#2e1fe6")
         .setTitle("Voici les animateurs de RadioLand avec leurs horaires :")
         .addField("Satyre59 :","```Dimanche 8h30-10h00\nVendredi 20h00-22h00\n Samedi 20h00-21h00```")
-        .addField ("Buxter_YTB :","```Lundi 8h00-10h00\nMardi 17h00-19h00\nsamedi 17h00-18h00\nDimanche 13h00-15h00```)
+        .addField ("Buxter_YTB :","```Lundi 8h00-10h00\nMardi 17h00-19h00\nsamedi 17h00-18h00\nDimanche 13h00-15h00```")
         .setFooter("RadioLand");
          message.channel.send({embed})
     }
