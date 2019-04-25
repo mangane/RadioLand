6bc67d1b9ff29a4e1644c182bd5e9df43c00704c
mangane <40431348+mangane@users.noreply.github.com>
2019-04-25 15:00:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,8 +15,10 @@ client.on('ready', () => {
         const embed = new Discord.RichEmbed()
         .setColor("#2e1fe6")
         .setTitle("Voici les animateurs de RadioLand avec leurs horaires :")
-        .addField("Satyre59 :","```Dimanche 8h30-10h00\nVendredi 20h00-22h00\n Samedi 20h00-21h00```")
-        .addField ("Buxter_YTB :","```Lundi 8h00-10h00\nMardi 17h00-19h00\nsamedi 17h00-18h00\nDimanche 13h00-15h00```")
+        .addField("Satyre59 :","```-Dimanche 8h30-10h00\n-Vendredi 20h00-22h00\n-Samedi 20h00-21h00```")
+        .addField ("Buxter_YTB :","```-Lundi 8h00-10h00\n-Mardi 17h00-19h00\n-samedi 17h00-18h00\n-Dimanche 13h00-15h00```")
+        .addField("Le duo : Sαтυиαe et Sophieskyyy :","```-Le Samedi 18h00-19h00\n-Le dimanche 11h00-12h00```")
+        .addField ("Mr Chat :","```-Vendredi 22h00-00h00\n-Mercredi 15h30-18h00```")
         .setFooter("RadioLand");
          message.channel.send({embed})
     }
