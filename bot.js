c47f225e67c7b649c21c3d7be39d9d923776cb02
mangane <40431348+mangane@users.noreply.github.com>
2019-04-25 15:38:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,8 @@ client.on('guildMemberRemove', member => {
   if(!channel) return;
     const embed = new Discord.RichEmbed()
     .setColor("#3ad729")
-    .addField(" :Bye: Une personne est partie, reviendra-t-elle ?", member.name)
+    .setTitle(" :Bye: Une personne est partie, reviendra-t-elle ?")
+    .addField("Au revoir :, member.name)
     .setFooter("RadioLand Au revoir");
 channel.send({embed})
 });
@@ -23,7 +24,8 @@ client.on('guildMemberAdd', member => {
   if (!channel) return;
     const embed = new Discord.RichEmbed()
     .setColor("#3ad729")
-    .addField(" :Bvn: Bienvenue sur le serveur :", member.name)
+    .setTitle(" :Bvn: Bienvenue sur le serveur :")
+    .addField("Welcome", member.name)
     .addFooter ("RadioLand Bienvenue");
 channel.send({embed})
 });
