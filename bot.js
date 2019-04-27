0dfff9288965e95fc5712514c06d016f73aee6d9
mangane <40431348+mangane@users.noreply.github.com>
2019-04-27 18:58:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,20 +30,6 @@ client.on('guildMemberAdd', member => {
 	channel.send({embed})
 });
   client.on(`message`, message =>{
-    if(message.content.startsWith(prefix + "anim")) {
-        message.delete()
-        const embed = new Discord.RichEmbed()
-        .setColor("#2e1fe6")
-        .setTitle("Voici les animateurs de RadioLand avec leurs horaires :")
-        .addField("Satyre59 :","```-Dimanche 8h30-10h00\n-Vendredi 20h00-22h00\n-Samedi 20h00-21h00```")
-        .addField ("Buxter_YTB :","```-Lundi 8h00-10h00\n-Mardi 17h00-19h00\n-samedi 17h00-18h00\n-Dimanche 13h00-15h00```")
-        .addField("Le duo : Sαтυиαe et Sophieskyyy :","```-Le Samedi 18h00-19h00\n-Le dimanche 11h00-12h00```")
-        .addField ("Mr Chat :","```-Vendredi 22h00-00h00\n-Mercredi 15h30-18h00```")
-        .addField ("AshOo :","```-Vendredi : 18h00-19h00\n-Samedi : 19h00-20h00```")
-	.addField("dιoғυɴ","```- Lundi 18h00-22h30\n-Jeudi : 17h00-22h30\n-Dimanche : 15h00-20h00```")
-        .setFooter("RadioLand");
-         message.channel.send({embed})
-    }
     if(message.content.startsWith(prefix + "count")) {
       message.delete ()
       message.reply(`Nous sommes ${message.guild.memberCount} Sur RadioLand`);
