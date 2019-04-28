3cd46e1a3d288c4bffab4005c4a241bf5bdf3453
mangane <40431348+mangane@users.noreply.github.com>
2019-04-28 06:04:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,7 +50,7 @@ client.on('guildMemberAdd', member => {
 		  const embed = new Discord.RichEmbed ()
 		  .setColor("0x954D23")
 		  .setTitle(`information sur un utilisateur du serveur`)
-		  .addField (`identifiant de la personne  :`,`${ruse}`);
+		  .addField (`identifiant de la personne  :`,`${ruse.usernames}`);
 		  message.channel.send({embed})
 }
   });
