3b0eda37b92d903a61fa79f9bb46c6dbcd58c518
mangane <40431348+mangane@users.noreply.github.com>
2019-04-28 09:49:59 +0200

    Add files via upload

---
+++ bot (2).js
@@ -0,0 +1,171 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "!";
+
+
+function return_date() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1;
+    var yyyy = today.getFullYear();
+    var hh = today.getHours();
+    var mn = today.getMinutes();
+    var ss = today.getSeconds();
+    var ms = today.getMilliseconds();
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    if (hh < 10) {
+        hh = '0' + hh;
+    }
+    if (mn < 10) {
+        mn = '0' + mn;
+    }
+    if (ss < 10) {
+        ss = '0' + ss;
+    }
+    today =  dd + '-' + mm + '-' + yyyy + ' et il est ' + hh + ':' + mm + ':' + ss + ':' + ms;
+    return today;
+}
+client.on('ready', () => {
+client.user.setStatus('idle')
+	.then(console.log)
+	.catch(console.error);
+});
+
+client.on('ready', () => {
+	client.user.setPresence({ game: { name: `Dans Ta Pub`, type: "LISTENING" } });
+});
+
+
+
+client.on('guildMemberRemove', member => {
+	
+	const channel = member.guild.channels.find(ch => ch.name === 'départ-arrivé');
+	if(!channel)return;
+	const embed = new Discord.RichEmbed()
+	.setColor("#3ad729")
+	.addField("Au revoir", `${member}`)
+	.setTitle(":outbox_tray: Une personne est partie, reviendra-t-elle ?:outbox_tray:");
+	channel.send({embed})
+});
+
+client.on('guildMemberAdd', member => {
+  console.log(return_date());
+  var date = return_date();
+  console.log(date);
+  const channel = member.guild.channels.find(ch => ch.name === 'départ-arrivé');
+    if (!channel)return;
+    const embed = new Discord.RichEmbed()
+    .setColor("#3ad729")
+    .setFooter("Nous sommes le : " + date)
+    .addField("Bienvenue", `${member}`)
+    .setTitle("📥 Bienvenue sur le serveur 📥");
+    channel.send({embed})
+});
+
+client.on("message", async message => {
+	/* KoS_ début du code =>
+		ajout d'un condition de bot et d'une vérification si y'a un préfixe ou pas
+	*/
+	if(message.author.bot) return;
+	if(message.content.indexOf(prefix) !== 0) return;
+	// ajout de args vu que tu appelle une variable qui n'existe pas
+	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+	// maintenant au lieu de mettre un if(message.content.startsWith(prefix + "commande") tu met in if(command === "commande")
+	// c'est plus clair non ?
+	if(command === "purge") {
+	const deleteCount = parseInt(args[0], 10);
+	if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+		return message.reply("Veuillez indiquer un nombre compris entre 2 et 100 pour le nombre de messages à supprimer.");
+		const fetched = await message.channel.fetchMessages({limit: deleteCount});
+		message.channel.bulkDelete(fetched)
+		.catch(error => message.reply(`Impossible de supprimer des messages à cause de: ${error}`));
+	}
+	if(command === "count") {
+		message.delete();
+		message.reply(`Nous sommes ${message.guild.memberCount} Sur RadioLand`);
+	}
+	if(command === "stats") {
+		message.delete()
+		const embed = new Discord.RichEmbed()
+		.setColor("#2e1fe6")
+		.setTitle("Statistiques")
+		.addField("Nombre de serveur :", client.guilds.size)
+		.addField("Nombre d'utilisateurs :", client.users.size);
+		message.channel.send({embed})
+	}
+	if(command === "pub") {
+		message.delete()
+		var amsg = message.content;
+		// supprime le premier mot de la chaine (string) donc "!pub machin" va supprimer pub
+		var msg = amsg.substr(amsg.indexOf(" ") + 1);
+		console.log(msg);
+		console.log(typeof msg);
+		// Donne le nombre de caractères dans la chaine
+		console.log(msg + ' ' + msg.length);
+		// ici c'est length pas size inférieur ou égal
+		if(msg.length <= 10) {
+			console.log("Votre publicité doit contenir plus de 10 caractère");
+			return message.channel.send ("Votre publicité doit contenir plus de 10 caractère");	
+
+		}
+		var reason = msg;
+		var test = message.guild.channels.find(`name`, "💫pub-vip💫");
+		console.log(test);
+		const embed = new Discord.RichEmbed()
+		.setColor("#15f153")
+		.setTitle("Publicité")
+		.addField("Publicité de :", `${message.author}`)
+		.addField("chan :", "💫pub-vip💫")
+		.addField("Publicité :", reason);
+		// envoie au channel courant
+		//message.channel.send({embed})
+		// envoie au channel distant par l'id
+		let chan = client.channels.find("id", "571904978936135696"); // Find the channel ID "123456789"
+	    if(chan) { // Check if that channel exists
+	        chan.send({embed});
+	    } else {
+	        message.channel.send("je ne trouve pas le salon de Pub contacter un administrateur! ");
+	    }
+
+		
+	}
+	// fin de l'ajout
+
+	// ton ancien code un peu bordélique
+	/*
+	if(message.content.startsWith(prefix + "count")) {
+		message.delete ()
+		message.reply(`Nous sommes ${message.guild.memberCount} Sur RadioLand`);
+	}
+	if(message.content.startsWith(prefix + "stats")) {
+		message.delete()
+		const embed = new Discord.RichEmbed()
+		.setColor("#2e1fe6")
+		.setTitle("Statistiques")
+		.addField("Nombre de serveur :", client.guilds.size)
+		.addField("Nombre d'utilisateurs :", client.users.size);
+		message.channel.send({embed})
+	}
+	if(message.content.startsWith(prefix + `pub`)) {
+		message.delete()
+	if(message.size === 10)
+		return mesage.channel.send ("Votre publicité doit contenir plus de 10 caractère")
+		let rreason = args.join(" ").slice(28);
+		let reportEmbed = new Discord.RichEmbed()
+		.setDescription("Publicité")
+		.setColor("#15f153")
+		.addField("Publicité de :", `${message.author}`)
+		.addField("Publicité :", rreason);
+		let reportschannel = message.guild.channels.find(`name`, "💫pub-vip💫");
+	if(!reportschannel) return message.channel.send("je ne trouve pas le salon de Pub contacter un administrateur! ");
+		reportschannel.send(reportEmbed);
+	}
+	*/
+  });
+client.login(BOT_TOKEN);
