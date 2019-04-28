edc4539e35119f03cc9daef5aa38150ce0afda15
mangane <40431348+mangane@users.noreply.github.com>
2019-04-28 16:54:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -139,7 +139,7 @@ message.delete ()
 const embed = new Discord.RichEmbed()
 .setColor ("#2e1fe6")
 .setTitle ("Bienvenue dans l'interface d'aide")
-.addField(`${message.author.avatarURL}`,`test`);
+.addField(`test`,`${message.author.avatarURL}`);
 message.channel.send ({embed})
 }
 	// fin de l'ajout
