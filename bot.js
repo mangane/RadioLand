b0e62b48000bdd188e9c4bb0ad4bfe06d7ce39ea
mangane <40431348+mangane@users.noreply.github.com>
2019-04-28 17:06:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -143,7 +143,7 @@ const embed = new Discord.RichEmbed()
 .setFooter(`demandé par ${message.author.username}`,`${message.author.avatarURL}`);
 message.channel.send ({embed})
 }
-	if(commmand = "sav") {
+	if(commmand === "sav") {
 message.reply ("Pour utiliser mon service de .pub tu devra tout d'abord créé un salon nommer **dans-ta-pub** ensuite il te suffira de faire la commande sur un salon et tu viens de finir le setup du .pub");
 }
 	// fin de l'ajout
