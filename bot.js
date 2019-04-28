b60fd7ed515d5aa8b7b49caa33db7429a21dba89
mangane <40431348+mangane@users.noreply.github.com>
2019-04-28 03:09:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ client.on('guildMemberAdd', member => {
     const embed = new Discord.RichEmbed()
     .setColor("#3ad729")
     .addField("Bienvenue", `${member}`)
-    .setFooter(`${guild.name}`)
+    .setFooter(`${message.guild.memberCount}`)
     .setTitle(":inbox_tray: Bienvenue sur le serveur :inbox_tray:");
 	channel.send({embed})
 });
