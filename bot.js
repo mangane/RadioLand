c687a31a7d79af0bca0e08c22f2e56d9ab64b59b
mangane <40431348+mangane@users.noreply.github.com>
2019-04-30 18:17:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -107,7 +107,11 @@ client.on("message", async message => {
 		.addField("Nombre de serveur :", client.guilds.size)
 		.addField("Nombre d'utilisateurs :", client.users.size);
 		message.channel.send({embed})
-	} else { cooldown.add(message.author.id); 
+	} else 
+		if (cooldown.has(message.author.id)) { 
+		message.channel.send("Attend une minute svp. - " + message.author); 
+					       }
+cooldown.add(message.author.id); 
 					   setTimeout(() => { 
 						   cooldown.delete(message.author.id); }, 60000); 
 						      }
