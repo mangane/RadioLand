07d50757523b7b360d2d12dc472510a605eb1cee
mangane <40431348+mangane@users.noreply.github.com>
2019-04-30 18:05:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -210,13 +210,12 @@ if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send(":
         message.delete(100);
         message.channel.send(`${m}`);
 		} 
-	if (cooldown.has(message.author.id)) { 
-		message.delete()
-		message.channel.send("Attend une minute svp. - " + message.author);
-		return message.delete()
-	} else { cooldown.add(message.author.id); 
-		setTimeout(() => { cooldown.delete(message.author.id); }, 60000); 
-	       }
+	cooldown.add(message.author.id);
+            
+            setTimeout(() => {
+
+         cooldown.delete(message.author.id);
+          }, 10000);
 	// fin de l'ajout
 
 	// ton ancien code un peu bordélique
