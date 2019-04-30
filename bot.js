24eb2ffeca1b4703421e3f248ae104f6e1900293
mangane <40431348+mangane@users.noreply.github.com>
2019-04-30 22:40:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -108,20 +108,7 @@ client.on("message", async message => {
 		.addField("Nombre d'utilisateurs :", client.users.size);
 		message.channel.send({embed})
 		message.delete()
-		const talkedRecently = new Set ();
-if (talkedRecently.has(msg.author.id)) {
-            msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
-    } else {
-
-           // the user can type the command ... your command code goes here :)
-
-        // Adds the user to the set so that they can't talk for a minute
-        talkedRecently.add(msg.author.id);
-        setTimeout(() => {
-          // Removes the user from the set after a minute
-          talkedRecently.delete(msg.author.id);
-        }, 1000);
-    }
+}
 	if(command === "pub") 
 		message.delete()
 		message.delete()
