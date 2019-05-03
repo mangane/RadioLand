f092a67469f12f16b35d5a35bae230fc0a032045
mangane <40431348+mangane@users.noreply.github.com>
2019-05-03 22:47:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,7 +42,7 @@ client.user.setStatus('idle')
 });
 client.on('ready', () => {
 var interval = setInterval(function () {
-client.channels.get("idDuChannel").setName("membres: " + client.users.size)
+client.channels.get("573972642172764164").setName("membres: " + client.users.size)
 }, 1*1000)
 client.on('ready', () => {
 	client.user.setPresence({ game: { name: `Dans Ta Pub | .help`, type: "WATCHING" } });
