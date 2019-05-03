263a19fe50ba6b085e202712a23aaa760f11a480
mangane <40431348+mangane@users.noreply.github.com>
2019-05-03 23:47:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,8 +40,8 @@ client.user.setStatus('idle')
 	.catch(console.error);
 });
 client.on('ready', () => {
+	console.log(return_dateFr());
 	var date = return_dateFr();
-	console.log(date());
 var interval = setInterval(function () {
 client.channels.get("573972642172764164").setName(date)
 }, 1*1000)
