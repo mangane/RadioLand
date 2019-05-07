6205c94bb5ff67258c73d3234f1ed57efdaf0017
mangane <40431348+mangane@users.noreply.github.com>
2019-05-07 21:20:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -147,8 +147,6 @@ if(command === "pub") {
 		//const channel = message.guild.channels.find(`name`,"dans-ta-pub");// Find the channel ID "123456789"
 	    //if(channel) { // Check if that channel exists
 	        client.channels.findAll('name', 'dans-ta-pub').map(channel => channel.send(embed))
-		    message.channel.send ("Votre publicité à été envoyer avec succès sur #dans-ta-pub");
-} else {
 if (command === "pub") { // Vérification. Est-ce que le membre est dans le cooldown.
     message.channel.send("⚠️ Le cooldown est activé pendant 2h ! ");
     }
@@ -157,6 +155,7 @@ cooldown.add(message.author.id);
 setTimeout(() => { 
     cooldown.delete(message.author.id); 
 }, 720000);
+		    message.channel.send ("Votre publicité à été envoyer avec succès sur #dans-ta-pub");
  }
 
 	if (command === "help") {
