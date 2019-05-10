1bc424e6953f2bd2b5238c929bab73fd35e78f49
mangane <40431348+mangane@users.noreply.github.com>
2019-05-10 17:32:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -152,12 +152,12 @@ if(command === "pub") {
 		    message.channel.send ("Votre publicité à été envoyer avec succès sur #dans-ta-pub");
 	    } else {
 	        message.channel.send("je ne trouve pas le salon de dans-ta-pub, contacter un administrateur! ");
+	    } else {
 	cooldown.add(message.author.id);
 setTimeout(() => { 
     cooldown.delete(message.author.id);
 }, 720000); 
  }
-	}
 	
 	if (command === "help") {
 const embed = new Discord.RichEmbed()
